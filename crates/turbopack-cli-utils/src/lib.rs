a237817b06755e28aa2ed98eaf57d819ec3fd0d6
Tobias Koppers <tobias.koppers@googlemail.com>
2023-11-15 11:54:11 +0000

    WIP

--- crates/turbopack-cli-utils/src/lib.rs
+++ crates/turbopack-cli-utils/src/lib.rs
@@ -5,14 +5,9 @@
 #![feature(arbitrary_self_types)]
 #![feature(async_fn_in_trait)]
 
-pub mod exit;
 pub mod issue;
-pub mod raw_trace;
 pub mod runtime_entry;
 pub mod source_context;
-pub mod trace_writer;
-pub mod tracing;
-pub mod tracing_presets;
 
 pub fn register() {
     turbo_tasks::register();
